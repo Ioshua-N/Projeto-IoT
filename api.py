544e76e1d413d83ea43490130184d1386d661dbf
Ioshua <126164539+Ioshua-N@users.noreply.github.com>
2024-05-21 10:51:23 -0300

    Update api.py

--- api.py
+++ api.py
@@ -1,8 +1,17 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app - Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
-    
+@app.route('/', methods = ['POST'])
+def receive_data():
+    if request.is_json:
+        data = request.get_json()
+        temperature = data.get('temperature')
+        humidity = data.get('dateTime')
+        id = data.get('id')
+
+        # mostrar valores no console
+        print(f'Temperatura: {temperature}')
+        print(f'Umidade: {humidity}')
+        print(f'Horário: {dateTime}')
+        print(f'Identificação: {id}')
