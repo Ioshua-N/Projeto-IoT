bce9822e4ae981aef5336c8f27444738f837b610
Ioshua <126164539+Ioshua-N@users.noreply.github.com>
2024-05-21 12:47:16 -0300

    v2

--- script.c
+++ script.c
@@ -3,62 +3,52 @@
 
 const char* ssid = "My ASUS";
 const char* password = "12345678";
-const char* serverName = "http://<SEU_IP>:8888";  // servidor Flask
-
-unsigned long lastTime = 0;
-unsigned long timerDelay = 30000;
-float temperature;
-float humidity;
+const char* serverName = "http://yourusername.pythonanywhere.com/";
 
 void setup()
 {
     Serial.begin(115200);
-    WiFi.begin(ssid, password);
 
+    WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED)
     {
         delay(1000);
-        Serial.println("Connecting to WiFi...");
+        Serial.println("Tentando conexão...");
     }
 
-    Serial.println("Connected to WiFi");
+    Serial.println("Conectado ao Wifi");
 }
 
 void loop()
 {
-    if ((millis() - lastTime) > timerDelay)
+    if (WiFi.status() == WL_CONNECTED)
     {
-        if (WiFi.status() == WL_CONNECTED)
+        HTTPClient http;
+
+        http.begin(serverName);
+        http.addHeader("Content-Type", "application/json");
+
+        float temperature = 25.0; // pegar do sensor
+        float humidity = 60.0; // pegar do sensor
+
+        String json = "{\"temperature\":" + String(temperature) + ",\"humidity\":" + String(humidity) + "}";
+
+        int httpResponseCode = http.POST(jsonPayload);
+
+        if(httpResponseCode > 0)
         {
-            HTTPClient http;
-
-            http.begin(serverName);
-            http.addHeader("Content-Type", "application/json");
-
-            temperature = 25.0;
-            humidity = 0.8;
-            String jsonData = "{\"temperature\":" + String(temperature) + ",\"humidity\":" + String(humidity) + "}";
-
-            int httpResponseCode = http.POST(jsonData);
-
-            if (httpResponseCode > 0)
-            {
-                String response = http.getString();
-                Serial.println(httpResponseCode);
-                Serial.println(response);
-            }
-            else
-            {
-                Serial.print("Error on sending POST: ");
-                Serial.println(httpResponseCode);
-            }
-
-            http.end();
+            String response = http.getString();
+            Serial.println(httpResponseCode);
+            Serial.println(response);
         }
         else
         {
-            Serial.println("WiFi Disconnected");
+            Serial.print("Error on sending POST: ");
+            Serial.println(httpResponseCode);
         }
-        lastTime = millis();
+
+        http.end();
     }
+
+    delay(1000); // enviar a cada 10s
 }
